e4253a13fecf67b9c66bb6803038bbe5a705e276
dima <pakilev08@mail.ru>
2021-07-12 22:01:10 +0800

    Bank 2.0

--- пробный.py
+++ пробный.py
@@ -2,13 +2,13 @@
 def new():
     global nums
     name = input("имя, фамилию, отчество: ")
-    with open('basa_name.txt') as basa_new:
+    with open('basa_coter.txt') as basa_new:
         if name in basa_new:
             print('Такой уже есть')
             return None
     yes = input(f"Вы точно хотите добавить {name}\n ")
     if yes == 'да':
-        with open('basa_name.txt', 'a') as basa_new:
+        with open('basa_coter.txt', 'a') as basa_new:
             basa_new.write(f'{name}')
             print(f'Вы добавили {name}')
             return None
@@ -24,23 +24,23 @@ def delete():
     print('Такого нет')
 def internet():
     name = input("имя, фамилию, отчество: ")
-    with open('basa_name.txt') as basa_new:
+    with open('basa_coter.txt') as basa_new:
         if name in basa_new:
             print('Есть такой')
             return None
         print("Таких нет")
 def oll():
-    with open('basa_name.txt') as basa_new:
+    with open('basa_coter.txt') as basa_new:
         for i in basa_new:
             print(f'{i}\n')
 def redact():
     name = input('Напишите старое имя, фамилию, отчество: ')
-    with open('basa_name.txt') as basa_new:
+    with open('basa_coter.txt') as basa_new:
         if name in basa_new:
             name_new = input("Введите новое имя, фамилию, отчество: ")
             yes = input(f'Вы точно хотите заминить {name} на {name_new}\n ')
             if yes == 'да':
-                with open('basa_name.txt', 'a') as basa_new:
+                with open('basa_coter.txt', 'a') as basa_new:
                     basa_new.write(name_new)
                     print(f'Вы заменили {name} на {name_new}')
                     return None
