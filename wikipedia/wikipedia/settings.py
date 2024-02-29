7328de138f6352a81970a677b85bb73bdda7e273
PakilevDima <84977657+PakilevDima@users.noreply.github.com>
2024-02-29 20:01:57 +0800

    Update settings.py

--- wikipedia/wikipedia/settings.py
+++ wikipedia/wikipedia/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nw_aq=^)jyg$15&rz&55wf&k@%nz3$lay5^p9k#g)fbj*7uh3o'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
